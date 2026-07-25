a45b0c467955e72675eb68e21dc39a6c2e0206a5
ahsub <ahildebrand@me.com>
2026-07-25 21:40:08 +0200

    fix(portfolioStore): remove duplicate isTestEnv declaration

--- src/stores/portfolioStore.js
+++ src/stores/portfolioStore.js
@@ -27,9 +27,6 @@ function calculatePnL(position, currentPrice) {
 
 const isTestEnv = process.env.NODE_ENV === 'test';
 
-
-const isTestEnv = process.env.NODE_ENV === 'test';
-
 const portfolioStoreCreator = (set, get) => ({
       isPaperMode: false,
       livePositions: [],
