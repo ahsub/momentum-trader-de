35a002706d40947744ed775c60ff38ff2f4eb81c
ahsub <ahildebrand@me.com>
2026-08-04 19:01:57 +0200

    Create index.ts to export tax strategies

---
+++ src/services/tax/strategies/index.ts
@@ -0,0 +1,6 @@
+// src/services/tax/strategies/index.ts
+export { BaseTaxStrategy } from './BaseTaxStrategy';
+export { EquityStrategy } from './EquityStrategy';
+export { CryptoStrategy } from './CryptoStrategy';
+export { PreciousMetalStrategy } from './PreciousMetalStrategy';
+export { DerivativeStrategy } from './DerivativeStrategy';
