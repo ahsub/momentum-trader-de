8a6341853e923b8cf4661dd0bef6a4d21d904aca
ahsub <ahildebrand@me.com>
2026-08-04 18:35:20 +0200

    Create index.ts

---
+++ src/services/tax/index.ts
@@ -0,0 +1,8 @@
+// src/services/tax/index.ts
+export { TaxEngine } from './TaxEngine';
+export { TaxStrategyFactory } from './TaxStrategyFactory';
+export { Portfolio } from './Portfolio';
+export { TaxConfigLoader } from './config/TaxConfigLoader';
+export * from './models';
+export * from './strategies';
+export type { TaxConfig } from './config/TaxConfigLoader';
